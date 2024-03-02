952d196bb740d7ccb5ff933f02fadbefb65fcf22
DmitriyW <dmitriy.belokon@gmail.com>
2024-03-03 00:50:14 +0200

    add first test for register admin handler

---
+++ tests/OnlineStore.AuthService.Core.UnitTests/Helpers/ServiceProviderBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using OnlineStore.AuthService.Models;
+
+namespace OnlineStore.AuthService.Core.UnitTests.Helpers;
+
+public class ServiceProviderBuilder
+{
+    private Mock<IServiceProvider> serviceProviderMock = new Mock<IServiceProvider>();
+    public Mock<UserManager<AuthUser>> UserManagerMock { get; } = new Mock<UserManager<AuthUser>>(Mock.Of<IUserStore<AuthUser>>(), null, null, null, null, null, null, null, null);
+    public Mock<RoleManager<IdentityRole>> RoleManagerMock { get; } = new Mock<RoleManager<IdentityRole>>(
+             new Mock<IRoleStore<IdentityRole>>().Object, null, null, null, null);
+
+    public ServiceProviderBuilder()
+    {
+        var serviceScope = new Mock<IServiceScope>();
+        serviceScope.Setup(x => x.ServiceProvider).Returns(serviceProviderMock.Object);
+
+        var serviceScopeFactory = new Mock<IServiceScopeFactory>();
+        serviceScopeFactory
+            .Setup(x => x.CreateScope())
+            .Returns(serviceScope.Object);
+
+        serviceProviderMock
+            .Setup(x => x.GetService(typeof(IServiceScopeFactory)))
+            .Returns(serviceScopeFactory.Object);
+
+        serviceProviderMock
+           .Setup(x => x.GetService(typeof(UserManager<AuthUser>)))
+           .Returns(UserManagerMock.Object);
+
+        serviceProviderMock
+            .Setup(x => x.GetService(typeof(RoleManager<IdentityRole>)))
+            .Returns(RoleManagerMock.Object);
+    }
+
+    public IServiceProvider BuildServiceProvider() => serviceProviderMock.Object;
+}
---
+++ tests/OnlineStore.AuthService.Core.UnitTests/RequestHandlers/RegisterAdmin/RegisterAdminHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using OnlineStore.AuthService.Core.Abstractions;
+using OnlineStore.AuthService.Core.Constants;
+using OnlineStore.AuthService.Core.Handlers.RegisterAdmin;
+using OnlineStore.AuthService.Core.UnitTests.Helpers;
+using OnlineStore.AuthService.Models;
+
+namespace OnlineStore.AuthService.Core.UnitTests.RequestHandlers.RegisterAdmin;
+
+public class RegisterAdminHandlerTests
+{
+    private Mock<IEmailService> emailServiceMock;
+    private ServiceProviderBuilder serviceProviderBuilder;
+    private RegisterAdminHandler registerAdminHandler;
+
+    public RegisterAdminHandlerTests()
+    {
+        serviceProviderBuilder = new ServiceProviderBuilder();
+
+        emailServiceMock = new Mock<IEmailService>();
+        emailServiceMock
+            .Setup(x => x.SendEmail(It.IsAny<AuthUser>(), It.IsAny<string>()))
+            .Returns(true);
+
+        registerAdminHandler = new RegisterAdminHandler(serviceProviderBuilder.BuildServiceProvider(), emailServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_WhenRegisterAdmin_ShouldReturnRegisterAdminResponse()
+    {
+        serviceProviderBuilder
+            .UserManagerMock
+                .Setup(x => x.CreateAsync(It.IsAny<AuthUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+        serviceProviderBuilder
+            .UserManagerMock
+            .Setup(x => x.GetUsersInRoleAsync(UserRoles.Admin))
+            .ReturnsAsync(new List<AuthUser>());
+
+        var request = new RegisterAdminRequest()
+        {
+            Username = "testName",
+            Email = "test@test",
+            Password = "testPsw",
+            ConfirmedPassword = "testPsw"
+        };
+
+        var result = await registerAdminHandler.Handle(request, CancellationToken.None);
+        Assert.NotNull(result);
+        Assert.IsType<RegisterAdminResponse>(result);
+    }
+}
