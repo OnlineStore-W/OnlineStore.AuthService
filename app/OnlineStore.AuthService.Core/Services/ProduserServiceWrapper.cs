3f60eed983e48263febcdaf79f10ca34b6a8589f
DmitriyW <dmitriy.belokon@gmail.com>
2024-03-03 20:14:16 +0200

    add produser wrapper

---
+++ app/OnlineStore.AuthService.Core/Services/ProduserServiceWrapper.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.AuthService.Core.Abstractions;
+using OnlineStore.CommonComponent.Kafka.Services;
+
+namespace OnlineStore.AuthService.Core.Services;
+
+public class ProduserServiceWrapper: IProduserServiceWrapper
+{
+    private readonly ProduserService produserService;
+
+    public ProduserServiceWrapper(ProduserService produserService)
+    {
+        this.produserService = produserService ?? throw new ArgumentNullException();
+    }
+
+    public async Task<bool> ProduceAsync<T>(T message) where T : class
+    {
+        await this.produserService.ProduceAsync(message);
+
+        return true;
+    }
+}
